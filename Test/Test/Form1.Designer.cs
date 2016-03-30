fe60a13667187eb4a45cc088e44f082a52e364eb
Vladimír Demko <vladimir.demko@akademiasovy.sk>
2016-03-30 14:19:26 +0200

    First Day

---
+++ Test/Test/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Answer1 = new System.Windows.Forms.TextBox();
+            this.Answer2R1 = new System.Windows.Forms.RadioButton();
+            this.Answer2R2 = new System.Windows.Forms.RadioButton();
+            this.Answer2R3 = new System.Windows.Forms.RadioButton();
+            this.IP1 = new System.Windows.Forms.CheckBox();
+            this.IP2 = new System.Windows.Forms.CheckBox();
+            this.IP3 = new System.Windows.Forms.CheckBox();
+            this.IP4 = new System.Windows.Forms.CheckBox();
+            this.Switch1 = new System.Windows.Forms.RadioButton();
+            this.Switch2 = new System.Windows.Forms.RadioButton();
+            this.Switch3 = new System.Windows.Forms.RadioButton();
+            this.Answer5 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1) First internet network was: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2) Which protocol works on transport overlay:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(14, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3) Which IP adress are right:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4) Switch works on:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(14, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(246, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "5) The most used type of PC network is:";
+            // 
+            // Answer1
+            // 
+            this.Answer1.Location = new System.Drawing.Point(204, 12);
+            this.Answer1.Name = "Answer1";
+            this.Answer1.Size = new System.Drawing.Size(100, 20);
+            this.Answer1.TabIndex = 5;
+            // 
+            // Answer2R1
+            // 
+            this.Answer2R1.AutoSize = true;
+            this.Answer2R1.Location = new System.Drawing.Point(0, 10);
+            this.Answer2R1.Name = "Answer2R1";
+            this.Answer2R1.Size = new System.Drawing.Size(46, 17);
+            this.Answer2R1.TabIndex = 6;
+            this.Answer2R1.TabStop = true;
+            this.Answer2R1.Text = "TCP";
+            this.Answer2R1.UseVisualStyleBackColor = true;
+            // 
+            // Answer2R2
+            // 
+            this.Answer2R2.AutoSize = true;
+            this.Answer2R2.Location = new System.Drawing.Point(112, 10);
+            this.Answer2R2.Name = "Answer2R2";
+            this.Answer2R2.Size = new System.Drawing.Size(35, 17);
+            this.Answer2R2.TabIndex = 7;
+            this.Answer2R2.TabStop = true;
+            this.Answer2R2.Text = "IP";
+            this.Answer2R2.UseVisualStyleBackColor = true;
+            // 
+            // Answer2R3
+            // 
+            this.Answer2R3.AutoSize = true;
+            this.Answer2R3.Location = new System.Drawing.Point(224, 10);
+            this.Answer2R3.Name = "Answer2R3";
+            this.Answer2R3.Size = new System.Drawing.Size(54, 17);
+            this.Answer2R3.TabIndex = 8;
+            this.Answer2R3.TabStop = true;
+            this.Answer2R3.Text = "HTTP";
+            this.Answer2R3.UseVisualStyleBackColor = true;
+            // 
+            // IP1
+            // 
+            this.IP1.AutoSize = true;
+            this.IP1.Location = new System.Drawing.Point(30, 136);
+            this.IP1.Name = "IP1";
+            this.IP1.Size = new System.Drawing.Size(95, 17);
+            this.IP1.TabIndex = 9;
+            this.IP1.Text = "127.35.259.14";
+            this.IP1.UseVisualStyleBackColor = true;
+            // 
+            // IP2
+            // 
+            this.IP2.AutoSize = true;
+            this.IP2.Location = new System.Drawing.Point(142, 136);
+            this.IP2.Name = "IP2";
+            this.IP2.Size = new System.Drawing.Size(89, 17);
+            this.IP2.TabIndex = 10;
+            this.IP2.Text = "192.168.1.10";
+            this.IP2.UseVisualStyleBackColor = true;
+            // 
+            // IP3
+            // 
+            this.IP3.AutoSize = true;
+            this.IP3.Location = new System.Drawing.Point(254, 136);
+            this.IP3.Name = "IP3";
+            this.IP3.Size = new System.Drawing.Size(71, 17);
+            this.IP3.TabIndex = 11;
+            this.IP3.Text = "10.2.4.10";
+            this.IP3.UseVisualStyleBackColor = true;
+            // 
+            // IP4
+            // 
+            this.IP4.AutoSize = true;
+            this.IP4.Location = new System.Drawing.Point(346, 136);
+            this.IP4.Name = "IP4";
+            this.IP4.Size = new System.Drawing.Size(101, 17);
+            this.IP4.TabIndex = 12;
+            this.IP4.Text = "105.14.255.999";
+            this.IP4.UseVisualStyleBackColor = true;
+            // 
+            // Switch1
+            // 
+            this.Switch1.AutoSize = true;
+            this.Switch1.Location = new System.Drawing.Point(0, 9);
+            this.Switch1.Name = "Switch1";
+            this.Switch1.Size = new System.Drawing.Size(57, 17);
+            this.Switch1.TabIndex = 13;
+            this.Switch1.TabStop = true;
+            this.Switch1.Text = "Fyzical";
+            this.Switch1.UseVisualStyleBackColor = true;
+            // 
+            // Switch2
+            // 
+            this.Switch2.AutoSize = true;
+            this.Switch2.Location = new System.Drawing.Point(113, 9);
+            this.Switch2.Name = "Switch2";
+            this.Switch2.Size = new System.Drawing.Size(45, 17);
+            this.Switch2.TabIndex = 14;
+            this.Switch2.TabStop = true;
+            this.Switch2.Text = "Line";
+            this.Switch2.UseVisualStyleBackColor = true;
+            // 
+            // Switch3
+            // 
+            this.Switch3.AutoSize = true;
+            this.Switch3.Location = new System.Drawing.Point(224, 13);
+            this.Switch3.Name = "Switch3";
+            this.Switch3.Size = new System.Drawing.Size(65, 17);
+            this.Switch3.TabIndex = 15;
+            this.Switch3.TabStop = true;
+            this.Switch3.Text = "Network";
+            this.Switch3.UseVisualStyleBackColor = true;
+            // 
+            // Answer5
+            // 
+            this.Answer5.FormattingEnabled = true;
+            this.Answer5.Items.AddRange(new object[] {
+            "Choose",
+            "Star-shaped",
+            "Fieldbus",
+            "Ring",
+            "Tree",
+            "Unlimited"});
+            this.Answer5.Location = new System.Drawing.Point(267, 215);
+            this.Answer5.Name = "Answer5";
+            this.Answer5.Size = new System.Drawing.Size(121, 21);
+            this.Answer5.TabIndex = 16;
+            this.Answer5.Text = "Choose";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(14, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "6) Official start of PC date:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(188, 251);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            this.dateTimePicker1.Value = new System.DateTime(2012, 12, 1, 0, 0, 0, 0);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Switch3);
+            this.groupBox1.Controls.Add(this.Switch2);
+            this.groupBox1.Controls.Add(this.Switch1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 179);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 30);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Answer2R1);
+            this.groupBox2.Controls.Add(this.Answer2R2);
+            this.groupBox2.Controls.Add(this.Answer2R3);
+            this.groupBox2.Location = new System.Drawing.Point(30, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(289, 29);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(16, 326);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 21;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Confirm
+            // 
+            this.Confirm.Location = new System.Drawing.Point(123, 326);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(75, 23);
+            this.Confirm.TabIndex = 22;
+            this.Confirm.Text = "Confirm";
+            this.Confirm.UseVisualStyleBackColor = true;
+            this.Confirm.Click += new System.EventHandler(this.Confirm_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(527, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Answer5);
+            this.Controls.Add(this.IP4);
+            this.Controls.Add(this.IP3);
+            this.Controls.Add(this.IP2);
+            this.Controls.Add(this.IP1);
+            this.Controls.Add(this.Answer1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Answer1;
+        private System.Windows.Forms.RadioButton Answer2R1;
+        private System.Windows.Forms.RadioButton Answer2R2;
+        private System.Windows.Forms.RadioButton Answer2R3;
+        private System.Windows.Forms.CheckBox IP1;
+        private System.Windows.Forms.CheckBox IP2;
+        private System.Windows.Forms.CheckBox IP3;
+        private System.Windows.Forms.CheckBox IP4;
+        private System.Windows.Forms.RadioButton Switch1;
+        private System.Windows.Forms.RadioButton Switch2;
+        private System.Windows.Forms.RadioButton Switch3;
+        private System.Windows.Forms.ComboBox Answer5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Confirm;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label7.Visible = false;
+        }
+        private string Answer1Right = "arpanet";
+        private string Answer5Right = "Star-shaped";
+        private string user1;
+        private string user5;
+        private System.DateTime rightDate = new System.DateTime(1970, 1, 1, 0, 0, 0);
+        private System.DateTime user6 = new DateTime();
+        int points = 0;
+        int wrong = 0;
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            Answer1.Text = "";
+            Answer2R1.Checked = Answer2R2.Checked = Answer2R3.Checked = false;
+            IP1.Checked = IP2.Checked = IP3.Checked = IP4.Checked = false;
+            Switch1.Checked = Switch2.Checked = Switch3.Checked = false;
+            Answer5.SelectedItem = "Choose";
+            dateTimePicker1.Text = "1. decembra 2012";
+        }
+
+        private void Confirm_Click(object sender, EventArgs e)
+        {
+            double percents;
+            points = 0;
+            wrong = 0;
+            //1
+            user1 = Answer1.Text.Trim();
+            user1.ToLower();
+            if (user1.Equals(Answer1Right))
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            //2
+            if (Answer2R1.Checked == true)
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            //3
+            if (IP1.Checked == false && IP2.Checked == true && IP3.Checked == true && IP4.Checked == false)
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            //4
+            if (Switch1.Checked)
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            //5
+            user5 = Answer5.Text;
+            if (user5.Equals(Answer5Right))
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            //6
+            user6 = dateTimePicker1.Value;
+            if (user6.Equals(rightDate))
+            {
+                points++;
+            }
+            else
+            {
+                wrong++;
+            }
+            percents = ((double)points / ((double)points + (double)wrong)) * 100;
+            label7.Text = "Your score: " + points.ToString()+"\nYour points in %: "+percents.ToString();
+            label7.Visible = true;
+        }
+    }
+}
